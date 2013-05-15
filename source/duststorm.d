738535e3a9a7acf1159aea65b0a161068b3a9cf5
1100110 <0b1100110@gmail.com>
2013-05-15 00:18:34 -0500

    basic skeleton

--- source/duststorm.d
+++ source/duststorm.d
@@ -1,7 +1,38 @@
 import vibe.d;
 
+// Create Immutable Data
+immutable VersionNumber = "0.0.1";
+immutable VersionName   = "duststorm";
 
+/// Startup Code
 shared static this()
 { 
-	logInfo("Edit source/app.d to start your project.");
+    // General Setup
+    setLogLevel( LogLevel.Debug );
+    setLogFile( "duststorm.log" );
+    
+    // Init Required Classes
+    auto settings = new HttpServerSettings;
+    auto router   = new UrlRouter;
+
+    // Init Required Settings
+    debug settings.port = 8080;
+    settings.errorPageHandler = toDelegate( &errorPage );
+   
+    // Setup Default Routing.
+    router.get( "*",    serveStaticFiles( "./public/" ));
+    router.get( "/",    staticTemplate!"index.dt" );
+
+    // Start the Event Loop
+    listenHttp( settings, router );
+}
+
+
+/// Handles Error page generation.
+void errorPage( HttpServerRequest   req, 
+                HttpServerResponse  res, 
+                HttpServerErrorInfo err) {
+    
+    res.renderCompat!("error.dt", HttpServerRequest,   "req",
+                                  HttpServerErrorInfo, "err")(req, err);
 }
